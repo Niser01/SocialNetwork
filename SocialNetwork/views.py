1a480dbb9a94eb428fff3613cb5495c313b110b4
Sergio Nicolás <shaldaneg@unal.edu.co>
2022-10-05 17:50:45 -0500

    Plantillas HTML inciales
    Plantillas inciales

--- SocialNetwork/views.py
+++ SocialNetwork/views.py
@@ -1,9 +1,17 @@
+from contextvars import Context
 from datetime import datetime
+from pipes import Template
 from django.http import HttpResponse
+from django.template import Template,Context
 import datetime
 
 def saludo(request):
-    return HttpResponse("Hola bienvenidos a su red social")
+    plantilla_index= open("C:/Users/Nicolas/Documents/GitHub/SocialNetwork/SocialNetwork/Plantillas/index.html")
+    plantilla_Template_index=Template(plantilla_index.read())
+    plantilla_index.close()
+    contx=Context()
+    Render_index=plantilla_Template_index.render(contx)
+    return HttpResponse(Render_index)
 
 def despedida(request):
     return HttpResponse("Hasta luego")
